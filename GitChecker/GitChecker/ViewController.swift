2247c2c3a861696b09c8e700f098f83dffaac993
Edgars Baumanis <edgars.baumanis@chi.lv>
2019-02-25 10:25:47 +0200

    Start

---
+++ GitChecker/GitChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitChecker
+//
+//  Created by Edgars Baumanis on 25.02.19.
+//  Copyright © 2019. g. chili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
