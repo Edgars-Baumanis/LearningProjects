4ba3e08eda1ca83cef46d911087ff736fbb0993e
Edgars Baumanis <edgars.baumanis@chi.lv>
2019-02-26 09:24:26 +0200

    made login and registration possible through FireBase

--- Event_Planner/Classes/Login/LogInVC.swift
+++ Event_Planner/Classes/Login/LogInVC.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
-
+    @IBOutlet weak var loginEmail: UITextField!
+    @IBOutlet weak var loginPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.setGradientBackground()
     }
     
     @IBAction func LoggedIn(_ sender: Any) {
-        NotificationCenter.default.post(name: Notification.Name("LoggedIn"), object: nil)
+        guard
+            let email = loginEmail.text,
+            let password = loginPassword.text
+        else { return }
+        if email.isEmpty && password.isEmpty { return }
+        else {
+            Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                if error != nil {
+                    print(error)
+                } else {
+                    NotificationCenter.default.post(name: Notification.Name("LoggedIn"), object: nil)
+                }
+            }
+        }
     }
-    
 }
