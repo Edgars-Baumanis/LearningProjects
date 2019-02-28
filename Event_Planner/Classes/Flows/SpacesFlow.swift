0b71c1d9ee33c58fc6db09bde384a2e4b0474e68
Edgars Baumanis <edgars.baumanis@chi.lv>
2019-02-28 18:02:15 +0200

    More code cleaning

--- Event_Planner/Classes/Flows/SpacesFlow.swift
+++ Event_Planner/Classes/Flows/SpacesFlow.swift
@@ -9,10 +9,9 @@
 import UIKit
 
 class SpacesFlow: FlowController {
-
-    private var rootController: UIViewController?
+    
     private var appWindow: UIWindow
-    var backToStart: (()->Void)?
+    var logoutPressed: (()->Void)?
     init (with window: UIWindow) {
         appWindow = window
     }
@@ -26,52 +25,57 @@ class SpacesFlow: FlowController {
         let tabbar = MainTabbarController()
         return tabbar
     }()
-
+    
     private lazy var joinVC: JoinASpaceController? = {
         return mainSB.instantiateViewController(withIdentifier: "JoinASpaceController") as? JoinASpaceController
     }()
     
-    lazy var mainVC: MySpacesVC? = {
+    lazy var spacesVC: MySpacesVC? = {
         return mainSB.instantiateViewController(withIdentifier: "MySpacesVC") as? MySpacesVC
     }()
     
     private lazy var createVC: CreateASpaceController? = {
         return mainSB.instantiateViewController(withIdentifier: "CreateASpaceController") as? CreateASpaceController
     }()
-
+    
+    private var rootController: UITabBarController!
     
     func start() {
-        guard let vc = tabbar else { return }
-        guard let createvc = createVC else {return}
+        guard let vc = tabbar else {return}
+        rootController = vc
+        guard let mySpacesVC = spacesVC else {return}
         appWindow.rootViewController = vc
         appWindow.makeKeyAndVisible()
-        initiateFirst()
-        initiateSecond()
-        initiateThird()
-        vc.viewControllers = [mainVC, createVC, joinVC] as? [UIViewController]
-        createvc.viewModel?.signingOut = { [weak self] in
-            self?.backToStart?()
+        initiateFirstVC()
+        initiateSecondVC()
+        vc.viewControllers = [mySpacesVC, joinVC] as? [UIViewController]
+        mySpacesVC.viewModel?.signingOut = { [weak self] in
+            self?.logoutPressed?()
+        }
+        mySpacesVC.viewModel?.navigateToCreate = { [weak self] in
+            self?.navigateToCreate()
         }
     }
-
-    func initiateFirst() {
-        guard let vc = mainVC else {return}
+    
+    func initiateFirstVC() {
+        guard let vc = spacesVC else {return}
         vc.viewModel = MySpacesModel()
         vc.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 0)
-        
     }
     
-    func initiateSecond() {
-        guard let vc = createVC else {return}
-        vc.viewModel = CreateASpaceModel()
-        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 1)
-    }
     
-    func initiateThird() {
+    func initiateSecondVC() {
         guard let vc = joinVC else {return}
         vc.viewModel = JoinASpaceModel()
         vc.tabBarItem = UITabBarItem(tabBarSystemItem: .mostRecent, tag: 2)
     }
+    
+    func navigateToCreate() {
+        guard let vc = createVC else {return}
+        vc.viewModel = CreateASpaceModel()
+        vc.viewModel?.backPressed = { [weak self] in
+            self?.rootController.dismiss(animated: true, completion: nil)
+        }
+        rootController.present(vc, animated: true, completion: nil)
+    }
 }
-
-
