12e3b26febedd1f25b3cd90d1adaef3234a433d5
Edgars Baumanis <edgars.baumanis@chi.lv>
2019-02-28 18:05:28 +0200

    More code cleanup

--- Event_Planner/Classes/Main/MySpaces/MySpacesVC.swift
+++ Event_Planner/Classes/Main/MySpaces/MySpacesVC.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import Firebase
 
 class MySpacesVC: UIViewController {
     var viewModel: MySpacesModel?
     override func viewDidLoad() {
         super.viewDidLoad()
         view.setGradientBackground()
+    }
+    
+    @IBAction func plusPressed(_ sender: Any) {
+        viewModel?.navigateToCreate?()
+    }
+    
+    @IBAction func signOutPressed(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            viewModel?.signingOut?()
+        } catch let signOutError as NSError {
+            print(signOutError)
+        }
     }
 }
 
@@ -21,7 +36,7 @@ extension MySpacesVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: (viewModel?.cellIdentifier)!, for: indexPath)
         if let myCell = cell as? MySpacesCell {
