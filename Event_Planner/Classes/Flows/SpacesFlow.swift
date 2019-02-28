15a7d3f307243c3c76162c6bb191b56cb8fe45e8
Edgars Baumanis <edgars.baumanis@chi.lv>
2019-02-28 15:55:25 +0200

    Log out option added on CreateASpace screen

--- Event_Planner/Classes/Flows/SpacesFlow.swift
+++ Event_Planner/Classes/Flows/SpacesFlow.swift
@@ -12,6 +12,7 @@ class SpacesFlow: FlowController {
 
     private var rootController: UIViewController?
     private var appWindow: UIWindow
+    var backToStart: (()->Void)?
     init (with window: UIWindow) {
         appWindow = window
     }
@@ -25,32 +26,7 @@ class SpacesFlow: FlowController {
         let tabbar = MainTabbarController()
         return tabbar
     }()
-    
-    func start() {
-        guard let vc = tabbar else { return }
-        appWindow.rootViewController = vc
-        appWindow.makeKeyAndVisible()
-        initiateFirst()
-        initiateSecond()
-        initiateThird()
-        vc.viewControllers = [mainVC, createVC, joinVC] as? [UIViewController]
-    }
 
-    func initiateFirst() {
-        guard let vc = mainVC else {return}
-        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 0)
-    }
-    
-    func initiateSecond() {
-        guard let vc = createVC else {return}
-        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 1)
-    }
-    
-    func initiateThird() {
-        guard let vc = joinVC else {return}
-        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .mostRecent, tag: 2)
-    }
-    
     private lazy var joinVC: JoinASpaceController? = {
         return mainSB.instantiateViewController(withIdentifier: "JoinASpaceController") as? JoinASpaceController
     }()
@@ -62,6 +38,40 @@ class SpacesFlow: FlowController {
     private lazy var createVC: CreateASpaceController? = {
         return mainSB.instantiateViewController(withIdentifier: "CreateASpaceController") as? CreateASpaceController
     }()
+
+    
+    func start() {
+        guard let vc = tabbar else { return }
+        guard let createvc = createVC else {return}
+        appWindow.rootViewController = vc
+        appWindow.makeKeyAndVisible()
+        initiateFirst()
+        initiateSecond()
+        initiateThird()
+        vc.viewControllers = [mainVC, createVC, joinVC] as? [UIViewController]
+        createvc.viewModel?.signingOut = { [weak self] in
+            self?.backToStart?()
+        }
+    }
+
+    func initiateFirst() {
+        guard let vc = mainVC else {return}
+        vc.viewModel = MySpacesModel()
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 0)
+        
+    }
+    
+    func initiateSecond() {
+        guard let vc = createVC else {return}
+        vc.viewModel = CreateASpaceModel()
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 1)
+    }
+    
+    func initiateThird() {
+        guard let vc = joinVC else {return}
+        vc.viewModel = JoinASpaceModel()
+        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .mostRecent, tag: 2)
+    }
 }
 
 
