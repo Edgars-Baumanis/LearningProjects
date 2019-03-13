9d951b43f3047a64b4ce7cbd42bfd733fcbeed1d
Edgars Baumanis <edgars.baumanis@chi.lv>
2019-03-13 11:38:44 +0200

    Added TableView to Chats

--- Event_Planner/Classes/Main/Chats/ChatsController.swift
+++ Event_Planner/Classes/Main/Chats/ChatsController.swift
@@ -13,18 +13,23 @@ class ChatsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.setGradientBackground()
-        // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ChatsController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ChatsCell.self), for: indexPath)
+        if let myCell = cell as? ChatsCell {
+            myCell.displayContent(chatName: "String")
+        }
+        return cell
+    }
+
 
 }
