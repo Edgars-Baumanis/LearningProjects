1c0c1dc3bc5648915b70ae05d2886bdb1ab7a883
Edgars Baumanis <edgars.baumanis@chi.lv>
2019-03-19 09:18:53 +0200

    added model for budget

---
+++ Event_Planner/Classes/Main/Budget/ConfigureBudget/ConfigureModel.swift
@@ -0,0 +1,18 @@
+//
+//  ConfigureModel.swift
+//  Event_Planner
+//
+//  Created by Edgars Baumanis on 15.03.19.
+//  Copyright © 2019. g. chili. All rights reserved.
+//
+
+import UIKit
+
+class ConfigureModel {
+    var fieldNames: [String] = []
+    var fieldSums: [Double] = []
+
+    func addField(name: String?, sum: Double?) {
+        
+    }
+}
